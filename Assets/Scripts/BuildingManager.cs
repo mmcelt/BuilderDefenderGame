b3c9028b45512cc459fd9a1282d96a064e4c2491
Ron Welch <mmcelt@yahoo.com>
2020-10-23 12:16:58 -0600

    8 - Place Building

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] Transform _woodHarvesterPrefab;
+
+	Camera _mainCamera;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		_mainCamera = Camera.main;
+	}
+	
+	void Update() 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Instantiate(_woodHarvesterPrefab, GetMouseWorldPosition(), Quaternion.identity);
+		}
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+	Vector3 GetMouseWorldPosition()
+	{
+		Vector3 mouseWorldPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+		mouseWorldPosition.z = 0;
+
+		return mouseWorldPosition;
+	}
+	#endregion
+}
